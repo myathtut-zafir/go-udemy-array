71451720009e509b340e90cd29519a40f5b65c07
myathtut-zafir <aa.htut@gmail.com>
2025-10-25 21:53:23 +0700

    feat: array spread

--- pratice.go
+++ pratice.go
@@ -23,11 +23,16 @@ func main() {
 	courseGoals[1] = "Master Go"
 	fmt.Println("Updated Course Goals:", courseGoals)
 
-	
 	products := []product{
 		{id: 1, name: "Laptop", price: 999.99},
 		{id: 2, name: "Smartphone", price: 499.99},
 	}
-	products= append(products, product{id: 3, name: "Tablet", price: 299.99})
+	products = append(products, product{id: 3, name: "Tablet", price: 299.99})
 	fmt.Println("Product Catalog:", products)
+
+	prices := []float64{19.99, 29.99, 39.99}
+	discountedPrices := []float64{17.99, 26.99}
+	prices = append(prices, discountedPrices...)
+	fmt.Println("All Prices:", prices)
+
 }
