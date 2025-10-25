23b40dad955c11443dff1d96fedf9540364f1ec3
myathtut-zafir <aa.htut@gmail.com>
2025-10-25 21:47:56 +0700

    feat: pratice array and slice

--- list.go
+++ list.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	prices := []float64{19.99, 29.99, 39.99}
-	var productNames []string
-	productNames = append(productNames, "hi", "hello", "yes")
-	// fmt.Println("Hello, World!", prices[0])
-	// fmt.Println("Hello, World!", productNames)
-	featurePrices := prices[1:3]
-	featurePrices = prices[:3]
-	fmt.Println("Hello, World!", featurePrices)
-	fmt.Println("Hello, World!", len(featurePrices))
-}
+// func main() {
+// 	prices := []float64{19.99, 29.99, 39.99}
+// 	var productNames []string
+// 	productNames = append(productNames, "hi", "hello", "yes")
+// 	// fmt.Println("Hello, World!", prices[0])
+// 	// fmt.Println("Hello, World!", productNames)
+// 	featurePrices := prices[1:3]
+// 	featurePrices = prices[:3]
+// 	fmt.Println("Hello, World!", featurePrices)
+// 	fmt.Println("Hello, World!", len(featurePrices))
+// }
+
+// func main() {
+// 	prices := []float64{19.99, 29.99, 39.99}
+// 	prices = append(prices, 5.0)
+// 	fmt.Println("Hello, World!", prices)
+// }
---
+++ pratice.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type product struct {
+	id    int
+	name  string
+	price float64
+}
+
+func main() {
+	hobbies := []string{"reading", "swimming", "coding"}
+	firstHobby := hobbies[0]
+	newHobbies := hobbies[1:3]
+
+	fmt.Println("Hobbies second and third:", newHobbies)
+	fmt.Println("Hobbies first:", firstHobby)
+
+	resliece := newHobbies[0:1]
+	fmt.Println("Reslice first element:", resliece)
+
+	courseGoals := []string{"Learn Go", "Build Projects"}
+	courseGoals[1] = "Master Go"
+	fmt.Println("Updated Course Goals:", courseGoals)
+
+	
+	products := []product{
+		{id: 1, name: "Laptop", price: 999.99},
+		{id: 2, name: "Smartphone", price: 499.99},
+	}
+	products= append(products, product{id: 3, name: "Tablet", price: 299.99})
+	fmt.Println("Product Catalog:", products)
+}
