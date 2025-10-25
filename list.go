aaa07557f8889878cc95ae9d82c2e2d39f8cd845
myathtut-zafir <aa.htut@gmail.com>
2025-10-25 21:03:52 +0700

    feat: array and slice

---
+++ list.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	prices := []float64{19.99, 29.99, 39.99}
+	var productNames []string
+	productNames = append(productNames, "hi", "hello", "yes")
+	// fmt.Println("Hello, World!", prices[0])
+	// fmt.Println("Hello, World!", productNames)
+	featurePrices := prices[1:3]
+	featurePrices = prices[:3]
+	fmt.Println("Hello, World!", featurePrices)
+
+}
