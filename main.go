7e3f228e25643aa36485aed7c823e3427f49a46a
myathtut-zafir <aa.htut@gmail.com>
2025-11-01 12:28:04 +0700

    feat: function return

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import "fmt"
+// this is function as deep dive
 
 type transformFn func(int) int
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-	double := transformNumber(&numbers, double)
+	numbers := []int{3, 2, 3, 4, 5}
+	newFunc := getTransformFn(&numbers)
+	double := transformNumber(&numbers, newFunc)
+
 	fmt.Println(double)
 }
 func transformNumber(numbers *[]int, transform transformFn) []int {
@@ -21,6 +24,13 @@ func double(number int) int {
 	return number * 2
 }
 
-func tripe(number int) int {
+func triple(number int) int {
 	return number * 3
+}
+
+func getTransformFn(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
 }
